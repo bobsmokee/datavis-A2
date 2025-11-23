6f30c3230eaff80d4ea81697b242fca0322b0664
Paul Heimer <heimerp54@gmail.com>
2025-11-23 12:50:45 +0100

    date

--- main.js
+++ main.js
@@ -1,46 +1,39 @@
-// Waiting until document has loaded
 window.onload = () => {
-
-  // YOUR CODE GOES HERE
-  console.log("YOUR CODE GOES HERE");
-
-  // Load the data set from the assets folder:
-
-  d3.csv("cars.csv").then(function(data) {
-    // Daten verarbeiten, z.B. Werte aus Strings in Zahlen konvertieren
+  d3.csv("cars.csv").then(data => {
     data.forEach(d => {
-      d.x = +d.x;  
+      d.x = +d.x;
       d.y = +d.y;
     });
-  
-    var svg = d3.select('svg'),
-        width = +svg.attr('width'),
-        height = +svg.attr('height');
 
-    // Skalen für Achsen definieren
+    var svg = d3.select("svg"),
+        margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = +svg.attr("width") - margin.left - margin.right,
+        height = +svg.attr("height") - margin.top - margin.bottom;
+
+    var g = svg.append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
     var xScale = d3.scaleLinear()
-      .domain([d3.min(data, d => d.x), d3.max(data, d => d.x)])
+      .domain(d3.extent(data, d => d.x))
       .range([0, width]);
-  
+
     var yScale = d3.scaleLinear()
-      .domain([d3.min(data, d => d.y), d3.max(data, d => d.y)])
+      .domain(d3.extent(data, d => d.y))
       .range([height, 0]);
-  
-    // Achsen zeichnen
-    svg.append("g")
-      .attr("transform", `translate(0, ${height})`)
+
+    g.append("g")
+      .attr("transform", `translate(0,${height})`)
       .call(d3.axisBottom(xScale));
-  
-    svg.append("g")
+
+    g.append("g")
       .call(d3.axisLeft(yScale));
-  
-    // Punkte zeichnen
-    svg.selectAll("circle")
+
+    g.selectAll("circle")
       .data(data)
-      .enter()
-      .append("circle")
+      .enter().append("circle")
       .attr("cx", d => xScale(d.x))
       .attr("cy", d => yScale(d.y))
-      .attr("r", 5);
+      .attr("r", 5)
+      .attr("fill", "steelblue");
   });
 };
